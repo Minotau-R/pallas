1390b3da1703a2a27feb2eb58d631f47caca83cc
thomaz <thomazbastiaanssen@gmail.com>
2025-10-20 23:49:24 +0200

    Improved `map_endpoint()`. Now adjusts `predicate()` hints based on mapped relations

--- R/map_endpoint.R
+++ R/map_endpoint.R
@@ -4,24 +4,70 @@
 #' Fetch information about known relational information from a sparql endpoint.
 #'
 #' @inheritParams send_query
+#' @param return.table `Boolean`. Whether to return the raw output table. For
+#'     development and debugging. (Default: `FALSE`).
 #' @export
 #' @returns a data frame with ?domain ?property ?range columns.
 #' @examples
 #' # Don't query endpoints unintentionally.
 #' if(FALSE) {
-#'     map_endpoint("https://sparql.uniprot.org/")
+#'     x <- map_endpoint("https://sparql.uniprot.org/")
 #' }
 #'
-map_endpoint <- function(endpoint_url) {
+map_endpoint <- function(endpoint_url, return.table = FALSE) {
     query <-
         "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX owl: <http://www.w3.org/2002/07/owl#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT ?domain ?property ?range
+SELECT ?domain ?range ?property
 WHERE {
     ?property rdf:type owl:ObjectProperty .
     ?property rdfs:range ?range .
     ?property rdfs:domain ?domain
 }"
-    send_query(query, endpoint_url, out_format = "text/csv")
+    x <- send_query(query, endpoint_url, out_format = "text/csv")
+
+    if(return.table) {return(x)}
+    # Else; (default)
+
+    URIs <- x
+    URIs[] <- lapply(x, .keep_URI)
+    x[]    <- lapply(x, .drop_URI)
+    missed <- x[URIs == x]
+    URI_sub <- sub(":.*$", "", missed)
+    x_sub   <- sub("^.*:", "", missed)
+    vocab <- `[<-`(x, URIs == x, value = x_sub)
+    URIs[URIs == x] <- URI_sub
+
+    all_URIs  <- unique(unlist(URIs, use.names = FALSE))
+
+    vocab_list <- lapply(
+        all_URIs,
+        function(x) unique(vocab[URIs == x])
+    )
+
+    names(vocab_list) <- all_URIs
+
+    pred_list <- lapply(
+        vocab_list,
+        function(y) (unique(y[y %in% x$property]))
+    )
+    pred_list <- pred_list[lengths(pred_list) != 0L]
+    pred_list <- lapply(pred_list, predicate_factory, vocab.table = vocab)
+
+    class_list <- lapply(
+        vocab_list,
+        function(y) (unique(y[y %in% c(vocab$domain, vocab$range)]))
+    )
+    class_list <- class_list[lengths(class_list) != 0L]
+
+    out_list <- list(
+        P = pred_list,
+        C = class_list
+    )
+
+    return( out_list )
 }
+
+.keep_URI <- function(x) sub("^(.*)/.*", "\\1", x)
+.drop_URI <- function(x) sub("^.*(.*)/", "\\1", x)
--- R/predicate_factory.R
+++ R/predicate_factory.R
@@ -3,6 +3,8 @@
 #' @param x `Character scalar`. Names of the desired predicate functions.
 #' @param to.list `Logical scalar`. Whether to return functions in a list
 #'     (`TRUE`, Default) or to the environment (`FALSE`).
+#' @param vocab.table `data.frame`. Used to customize input hints. Output of
+#'     `map_endpoint` with `return.table` set to `TRUE`. (Default: NULL).
 #' @returns A list of functions, or to the environment.
 #' @examples
 #' P <- predicate_factory(x = c("predicate_A", "predicate_B", "predicate_C"))
@@ -13,21 +15,45 @@
 #'
 #' @export
 #'
-predicate_factory <- function(x, to.list = TRUE) {
+predicate_factory <- function(x, to.list = TRUE, vocab.table = NULL) {
     if(to.list) {
-        pred_list <- lapply(x, .assign_pred)
+        pred_list <- lapply(x, .assign_pred, vocab.table)
         names(pred_list) <- x
         return(pred_list)
     } else {
         for (i in x) {
-            assign(i, .assign_pred(i) )
+            assign(i, .assign_pred(i, vocab.table) )
         }
     }
 }
 
-.assign_pred <- function(i) {
+
+#' @importFrom rlang fn_fmls<-
+#' @importFrom stats reformulate
+.assign_pred <- function(i, vocab.table) {
     force(i)
-    function(x) predicate.formula(x, what = i)
+    if(is.null(vocab.table)) {
+        return (
+            function(... = subject ~ object ) {
+                predicate.formula(..., what = i)
+            }
+        )
+    }
+    # Else
+    force(vocab.table)
+    hint <- vocab.table[vocab.table$property == i, c(1, 2)]
+    hint <- reformulate(
+        paste0(hint$range, collapse = " "),
+        paste0(hint$domain, collapse = " ")
+    )
+    # Return a wrapper around predicate.formula.
+    # Adjust formals to contain hint.
+    rlang::`fn_fmls<-`(
+        function(... = subject ~ object ) {
+            predicate.formula(..., what = i)
+        },
+        value = list("..." = hint)
+    )
 }
 
 
